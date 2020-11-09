2f0da818b3714f95e653bef193947b3d70d85ff5
Wasswa Richard <wasswa@awamo.com>
2020-11-09 05:37:03 +0300

    deploying to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,8 @@ export const environment = {
   production: true,
   version: env.npm_package_version,
   fineractPlatformTenantId: 'default',  // For connecting to server running elsewhere update the tenant identifier
-  baseApiUrl: 'https://demo.fineract.dev',  // For connecting to server running elsewhere update the base API URL
+  // baseApiUrl: 'https://demo.fineract.dev',  // For connecting to server running elsewhere update the base API URL
+  baseApiUrl: 'https://159.65.214.23:8443',
   apiProvider: '/fineract-provider/api',
   apiVersion: '/v1',
   serverUrl: '',
