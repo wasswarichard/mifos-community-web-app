1644c2b387de2b4d629b9c9bf422155f9ad1b7f2
Wasswa Richard <wasswa@awamo.com>
2020-11-09 06:06:04 +0300

    deploying to heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/mifos-community-web-app'));
+app.use(express.static(__dirname + '/dist/web-app'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/mifos-community-web-app/index.html'));});
+    '/dist/web-app/index.html'));});
 app.listen(process.env.PORT || 8080);
 
 //    "start": "npm run env -s && ng serve --aot --proxy-config proxy.conf.js",
